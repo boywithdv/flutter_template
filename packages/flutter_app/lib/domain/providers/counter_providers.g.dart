2389b2ef1413ac69d695b9c34826054ddd579f93
jukiya <jukiya8891@gmail.com>
2025-09-01 10:22:46 +0900

    chore: melos gen差分

--- packages/flutter_app/lib/domain/providers/counter_providers.g.dart
+++ packages/flutter_app/lib/domain/providers/counter_providers.g.dart
@@ -6,83 +6,198 @@ part of 'counter_providers.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$counterDataSourceHash() => r'5b8f0e97780bd017df4425bba4baf54db976642f';
+/// Provider for CounterDataSource
+/// This is a singleton provider to maintain state
+@ProviderFor(counterDataSource)
+const counterDataSourceProvider = CounterDataSourceProvider._();
 
 /// Provider for CounterDataSource
 /// This is a singleton provider to maintain state
-///
-/// Copied from [counterDataSource].
-@ProviderFor(counterDataSource)
-final counterDataSourceProvider = Provider<CounterDataSource>.internal(
-  counterDataSource,
-  name: r'counterDataSourceProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$counterDataSourceHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+final class CounterDataSourceProvider
+    extends
+        $FunctionalProvider<
+          CounterDataSource,
+          CounterDataSource,
+          CounterDataSource
+        >
+    with $Provider<CounterDataSource> {
+  /// Provider for CounterDataSource
+  /// This is a singleton provider to maintain state
+  const CounterDataSourceProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'counterDataSourceProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef CounterDataSourceRef = ProviderRef<CounterDataSource>;
-String _$counterRepositoryHash() => r'eeb1e0032f9d97d3e47682445164f2a2684f3139';
+  @override
+  String debugGetCreateSourceHash() => _$counterDataSourceHash();
+
+  @$internal
+  @override
+  $ProviderElement<CounterDataSource> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  CounterDataSource create(Ref ref) {
+    return counterDataSource(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(CounterDataSource value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<CounterDataSource>(value),
+    );
+  }
+}
+
+String _$counterDataSourceHash() => r'5b8f0e97780bd017df4425bba4baf54db976642f';
 
 /// Provider for CounterRepository
-///
-/// Copied from [counterRepository].
 @ProviderFor(counterRepository)
-final counterRepositoryProvider = Provider<CounterRepository>.internal(
-  counterRepository,
-  name: r'counterRepositoryProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$counterRepositoryHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+const counterRepositoryProvider = CounterRepositoryProvider._();
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef CounterRepositoryRef = ProviderRef<CounterRepository>;
-String _$counterHash() => r'e950a5b93603e9bb13cf68ae27b27e3cb1845aa5';
+/// Provider for CounterRepository
+final class CounterRepositoryProvider
+    extends
+        $FunctionalProvider<
+          CounterRepository,
+          CounterRepository,
+          CounterRepository
+        >
+    with $Provider<CounterRepository> {
+  /// Provider for CounterRepository
+  const CounterRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'counterRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$counterRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<CounterRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  CounterRepository create(Ref ref) {
+    return counterRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(CounterRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<CounterRepository>(value),
+    );
+  }
+}
+
+String _$counterRepositoryHash() => r'eeb1e0032f9d97d3e47682445164f2a2684f3139';
 
 /// FutureProvider for getting counter value
 /// This demonstrates Query/Subscription pattern
-///
-/// Copied from [counter].
 @ProviderFor(counter)
-final counterProvider = AutoDisposeFutureProvider<Counter>.internal(
-  counter,
-  name: r'counterProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$counterHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+const counterProvider = CounterProvider._();
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef CounterRef = AutoDisposeFutureProviderRef<Counter>;
-String _$counterNotifierHash() => r'29f847ba27495d72e71ad466a1d74aec0bd34ef6';
+/// FutureProvider for getting counter value
+/// This demonstrates Query/Subscription pattern
+final class CounterProvider
+    extends $FunctionalProvider<AsyncValue<Counter>, Counter, FutureOr<Counter>>
+    with $FutureModifier<Counter>, $FutureProvider<Counter> {
+  /// FutureProvider for getting counter value
+  /// This demonstrates Query/Subscription pattern
+  const CounterProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'counterProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$counterHash();
+
+  @$internal
+  @override
+  $FutureProviderElement<Counter> $createElement($ProviderPointer pointer) =>
+      $FutureProviderElement(pointer);
+
+  @override
+  FutureOr<Counter> create(Ref ref) {
+    return counter(ref);
+  }
+}
+
+String _$counterHash() => r'e950a5b93603e9bb13cf68ae27b27e3cb1845aa5';
 
 /// Provider for counter increment operation
 /// This demonstrates Mutation pattern
-///
-/// Copied from [CounterNotifier].
 @ProviderFor(CounterNotifier)
-final counterNotifierProvider =
-    AsyncNotifierProvider<CounterNotifier, Counter>.internal(
-      CounterNotifier.new,
-      name: r'counterNotifierProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$counterNotifierHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
+const counterNotifierProvider = CounterNotifierProvider._();
 
-typedef _$CounterNotifier = AsyncNotifier<Counter>;
+/// Provider for counter increment operation
+/// This demonstrates Mutation pattern
+final class CounterNotifierProvider
+    extends $AsyncNotifierProvider<CounterNotifier, Counter> {
+  /// Provider for counter increment operation
+  /// This demonstrates Mutation pattern
+  const CounterNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'counterNotifierProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$counterNotifierHash();
+
+  @$internal
+  @override
+  CounterNotifier create() => CounterNotifier();
+}
+
+String _$counterNotifierHash() => r'29f847ba27495d72e71ad466a1d74aec0bd34ef6';
+
+abstract class _$CounterNotifier extends $AsyncNotifier<Counter> {
+  FutureOr<Counter> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<AsyncValue<Counter>, Counter>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<Counter>, Counter>,
+              AsyncValue<Counter>,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
+
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
